9965df22bcc992cf8d9c6ba24c5c943e4165cfa6
AwesomeSeal <Supersayon40@gmail.com>
2023-02-23 08:45:48 -0600

    Add files via upload

--- js/reset/reset.js
+++ js/reset/reset.js
@@ -3,15 +3,15 @@ let GameWins = {
     return player.gameWins;
   },
   multiplier() {
-    return this.amount();
-  },
-  restart() {
-	let oldWins = player.gameWins
-	Saving.resetGameWithConfirmation();
-	player.gameWins = oldWins
+    return Math.pow(1.05, this.amount());
   },
   increment() {
+	if (confirm('Do you really want to reset the game? You will lose all your progress, and get a 1.05x multiplier.')) {
+    Notifications.notify('Game has been reset!', 'saveLoad');
     player.gameWins++;
-	this.restart();
+	let oldWins = player.gameWins
+    Saving.resetGame();
+	player.gameWins = oldWins
+	}
   },
 }
