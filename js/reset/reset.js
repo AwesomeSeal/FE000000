990cd79ca09101631d5553d690bf92e415ca5e13
AwesomeSeal <Supersayon40@gmail.com>
2023-08-22 11:34:30 -0500

    Update reset.js
    Undo

--- js/reset/reset.js
+++ js/reset/reset.js
@@ -5,9 +5,6 @@ let GameWins = {
   multiplier() {
     return Math.pow(2, this.amount());
   },
-  multiplierTwo() {
-    return 1 / this.amount();
-  },
   increment() {
 	if (confirm('Do you really want to reset the game? You will lose all your progress, and get a 2x multiplier.')) {
     Notifications.notify('Game has been reset!', 'saveLoad');
