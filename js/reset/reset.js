8c645ddb7bb7fb6fe9540b0fcbb56a7183ad11a8
AwesomeSeal <Supersayon40@gmail.com>
2023-08-22 11:24:14 -0500

    Update reset.js
    Added multipliertwo

--- js/reset/reset.js
+++ js/reset/reset.js
@@ -5,8 +5,11 @@ let GameWins = {
   multiplier() {
     return Math.pow(2, this.amount());
   },
+  multiplierTwo() {
+    return 1 / this.amount();
+  },
   increment() {
-	if (confirm('Do you really want to reset the game? You will lose all your progress, and get a 1.2x multiplier.')) {
+	if (confirm('Do you really want to reset the game? You will lose all your progress, and get a 2x multiplier.')) {
     Notifications.notify('Game has been reset!', 'saveLoad');
     player.gameWins++;
 	let oldWins = player.gameWins
